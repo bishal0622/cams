64f264cc833212880fda216fe53a4ea561748f12
japnika <japnika.dangol@gmail.com>
2017-01-16 17:37:35 +0545

    test

---
+++ api/src/main/java/com/clinic/web/rest/LoginController.java
@@ -0,0 +1,37 @@
+package com.clinic.web.rest;
+
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by japnica on 1/15/2017.
+ */
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public LoginController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
+    }
+
+    @GetMapping
+    public String helloFacebook(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+        PagedList<Post> feed = facebook.feedOperations().getFeed();
+        model.addAttribute("feed", feed);
+        return "hello";
+    }
+}
